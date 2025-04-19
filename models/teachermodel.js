4f1f7d66cc6b4fc0719ce285c0bfc56ed696debc
Anand Goyal <anandgoyal794@gmail.com>
2025-04-20 00:29:04 +0530

    add project files

---
+++ models/teachermodel.js
@@ -0,0 +1,48 @@
+const mongoose=require('mongoose');
+const Class=require("./classmodel.js");
+const schema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+    },
+    phone:{
+        type:Number,
+        required:true,
+    }
+    ,image:{
+        type:String,
+    }
+    ,
+    address:{
+        type:String,
+    }
+    ,
+    classes:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Class",
+    }],
+    subjects:[
+        {type:String}
+    ],
+    qualification:[
+        {type:String}
+    ],
+    experience:[{
+        type:String,
+    }],
+    Dateofjoining:{
+        type:String,
+    },
+
+    
+
+
+
+
+
+})
+const Teacher=mongoose.model('Teacher',schema);
+module.exports=Teacher;
